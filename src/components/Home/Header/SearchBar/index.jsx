741c554014c23286ba823e065422b11563e29aea
Stefan Brunotte <mrbrunotte@gmail.com>
2023-01-17 21:30:52 +0100

    update

--- src/components/Home/Header/SearchBar/index.jsx
+++ src/components/Home/Header/SearchBar/index.jsx
@@ -7,7 +7,7 @@ const SearchBar = ({value, handleSearchKey, clearSearch, formSubmit}) => (
         <input 
         type='text'
         onChange={handleSearchKey} 
-        placeholder='Type instrument category' 
+        placeholder='Type instrument ES/MES/6E etc...' 
         value={value}
         />
         {value && <span onClick={clearSearch}>X</span>}
